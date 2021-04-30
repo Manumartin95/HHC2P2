e91af43b096a202fbec184efdb1780c11cb59179
mmartin <manuel-harley@hotmail.com>
2021-04-30 14:44:45 +0100

    Fifth commit

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,6 +3,7 @@
  * see commented examples below
  */
  import * as bootstrap from 'bootstrap';
+ 
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
 
@@ -10,12 +11,7 @@
  * Write any other JavaScript below
  */
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
-
-$(document).ready(function(){
+$(function(){
   $('[data-toggle="popover"]').popover({
       trigger:'hover',
       content:'El código CVV o CVC es un grupo de 3 o 4 números situado en el reverso de la tarjeta de crédito.',
